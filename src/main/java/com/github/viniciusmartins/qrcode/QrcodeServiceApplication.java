f1349ae326b7200740762b3a4338de23d413cd88
Vinicius Martins <vinicius.martins@tqi.com.br>
2024-11-03 12:03:18 -0300

    first commit: spring app and gradle

---
+++ src/main/java/com/github/viniciusmartins/qrcode/QrcodeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.github.viniciusmartins.qrcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrcodeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QrcodeServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/viniciusmartins/qrcode/QrcodeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.viniciusmartins.qrcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrcodeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
