d88642a7c4e6e58202e828caaa51e00c0cff97ee
Vinicius Martins <vinicius.martins@tqi.com.br>
2024-11-06 13:16:56 -0300

    test: testes unitarios das validacoes

---
+++ src/test/java/com/github/viniciusmartins/qrcode/unity/validation/QRCodeStatusValidationTest.java
@@ -0,0 +1,27 @@
+package com.github.viniciusmartins.qrcode.unity.validation;
+
+import com.github.viniciusmartins.qrcode.exception.BadRequestException;
+import com.github.viniciusmartins.qrcode.exception.ErrorCodeEnum;
+import com.github.viniciusmartins.qrcode.validation.QRCodeStatusValidation;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+@ExtendWith(MockitoExtension.class)
+public class QRCodeStatusValidationTest {
+
+    @Test
+    public void validateStatusTest() {
+        try {
+            QRCodeStatusValidation.validateStatus("invalidStatus", "status");
+            fail("Testing exception: test shouldn't get to this line");
+        } catch (BadRequestException e) {
+            assertEquals(ErrorCodeEnum.INVALID_FIELD, e.getErrorDTO().code());
+            assertEquals("invalid.field.status", e.getErrorDTO().message());
+        }
+    }
+
+}
